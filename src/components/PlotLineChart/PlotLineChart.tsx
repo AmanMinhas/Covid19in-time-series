3b8dc248cc474cc5ed0acfa43426f49274de8db7
AmanMinhas <amandeepSinghMinhas@gmail.com>
2020-04-27 20:01:21 +0530

    [C19-2] Setup Region Selection

--- src/components/PlotLineChart/PlotLineChart.tsx
+++ src/components/PlotLineChart/PlotLineChart.tsx
@@ -1,28 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { LineChart, CartesianGrid, XAxis, YAxis, Legend, Tooltip, Line } from 'recharts';
-import { selectedStates } from './selectedStates';
+import { LineChart, CartesianGrid, XAxis, YAxis, Legend, Tooltip, Line, ResponsiveContainer } from 'recharts';
+import { IStats, ISelectedState, IDayData, IRegion } from '../../pages/Home/Home';
+// import { selectedStates } from './selectedStates';
+import './PlotLineChart.scss';
 
 interface PlotLineChartProps {
-  statesData: any;
+  statesData: IStats;
+  selectedStates: ISelectedState[];
 }
 
 const PlotLineChart = (props: PlotLineChartProps) => {
-  const { statesData } = props;
-  const [lineData, setLineData] = useState([]);
-  const plotCountry = true;
+  const { statesData, selectedStates } = props;
+  const [lineData, setLineData] = useState<any>([]);
+  const className = 'c-PlotLineChart';
 
   useEffect(() => {
     if (statesData) {
       const { data } = statesData;
 
-      const formattedLineData = data.map((dayData: any) => {
+      const formattedLineData = data.map((dayData: IDayData) => {
         const { day, summary, regional } = dayData;
-        const countryData = {
-          india: summary.total - summary.discharged
-        };
+        const shortDay = day
+          .replace('2020-', '')
+          .replace('-', '/');
+
+        let includeCountryData = false;
         let statesData = {};
-        selectedStates.forEach((state) => {
-          const details = regional.find((region: any) => region.loc === state.name) || { totalConfirmed: 0, discharged: 0 };
+        selectedStates.forEach((state: ISelectedState) => {
+          if (state.name === 'India') {
+            includeCountryData = true;
+            return;
+          }
+          const details = regional.find((region: IRegion) => region.loc === state.name) || { totalConfirmed: 0, discharged: 0 };
           const { totalConfirmed, discharged } = details;
           statesData = {
             ...statesData,
@@ -30,47 +39,46 @@ const PlotLineChart = (props: PlotLineChartProps) => {
           };
         })
 
+        const countryData = includeCountryData ? {
+          India: summary.total - summary.discharged
+        } : {};
+
         return {
-          day,
+          day: shortDay,
           ...countryData,
           ...statesData
         }
       });
 
-      console.log('formattedLineData ', formattedLineData);
       setLineData(formattedLineData);
     }
-  }, [statesData])
+  }, [statesData, selectedStates])
 
   if (!lineData.length) return null;
   return (
-    <div>
-      <LineChart width={730} height={250} data={lineData}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="day" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        {plotCountry && <Line
-          type="monotone"
-          dataKey='india'
-          stroke='red'
-        />}
-        {selectedStates.map((stateData) => {
-          const { dataKey, color } = stateData;
-          return (
-            <Line
-              key={dataKey}
-              type="monotone"
-              dataKey={dataKey}
-              stroke={color}
-              dot={false}
-            />
-          )
-        })}
-
-      </LineChart>
+    <div className={className}>
+      <ResponsiveContainer width='100%' height={250}>
+        <LineChart width={730} height={250} data={lineData}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="day" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          {selectedStates.map((stateData) => {
+            const { dataKey, color } = stateData;
+            return (
+              <Line
+                key={dataKey}
+                type="monotone"
+                dataKey={dataKey}
+                stroke={color}
+                dot={false}
+              />
+            )
+          })}
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 }
